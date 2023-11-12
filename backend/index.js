9055c3db45884e193d027eb2ee1ce9fca0412f10
Nikodem Młyński <nikodemmlynski08@gmail.com>
2023-11-12 21:34:38 +0100

    TO NIE ZMIENIA FUNKCJONALNOSCI APLIKACJI dodanie funkcji akceptujacej cors na serwer

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,12 @@ const client = new MongoClient(url);
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://51.77.48.162');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.get("/", (req, res) =>{
     res.send(`Serwer działa`);
